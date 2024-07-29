4c230fd60d92d8a9e230928637dbf9733df0fbf6
Eduardo Linux Acer <tardineduardo@gmail.com>
2024-07-29 21:58:14 +0200

    final

--- get_next_line.c
+++ get_next_line.c
@@ -14,10 +14,17 @@ static char	*ft_read_loop(int fd, char *new)
 			return (NULL);
 		rsize = read(fd, buf, BUFFER_SIZE);
 		if (rsize == -1)
+		{
+			free(buf);
 			return(NULL);
+		}
 		new = ft_strjoin(new, buf);
+		
 		if (ft_strchr(buf, '\n'))
+		{
+			free(buf);
 			break;
+		}
 		free(buf);
 	}
 	return(new);
@@ -25,8 +32,10 @@ static char	*ft_read_loop(int fd, char *new)
 
 static char	*ft_extract_remain(char *new, char *rem)
 {
-	int a;
+	int		a;
+	char	*temp;
 
+	temp = rem;
 	rem = ft_calloc(BUFFER_SIZE, 1);
 	if (!rem)
 		return (NULL);
@@ -35,6 +44,7 @@ static char	*ft_extract_remain(char *new, char *rem)
 		a++;
 	ft_strlcpy(rem, &new[a + 1], BUFFER_SIZE);
 	new[a + 1] = 0;
+	free(temp);
 	return(rem);
 }
 
@@ -63,15 +73,20 @@ static char	*ft_handle_rem(char *rem)
 
 char *get_next_line(int fd)
 {
-    char           *new;
-    static char    *rem;
+    char			*new;
+	char			*temp;
+    static char		*rem;
 
 	new = ft_calloc(1, 1);
+	temp = new;
 	if(rem && rem[0] != 0)
 	{	
 		new = ft_handle_rem(rem);
 		if (ft_strchr(new, '\n') || rem[0] == 0)
+		{
+			free(temp);
 			return(new);
+		}
 	}
     new = ft_read_loop(fd, new);
     if(!new || new[0] == 0)
@@ -83,7 +98,6 @@ char *get_next_line(int fd)
     }
 	if (ft_strchr(new, '\n'))	
 		rem = ft_extract_remain(new, rem);
-
 	return (new);
 }
 
@@ -95,19 +109,23 @@ int main(void)
     int a = 0; // Counter for lines
     int b = 1; // Line number for printing
     char *newline; // Pointer to store the line read
-	newline = calloc(1, 1);
+	char *temp;
+	newline = malloc (1*1);
+	temp = newline;
     while(newline) 
     {
         newline = get_next_line(file); // Get the next line from the file
         if (newline == NULL) // If NULL is returned (EOF or error)
 		{
-	        break; // Exit the loop
+			break; // Exit the loop
 		}
         printf("newline %d -> %s", b, newline); // Print the line
-        free(newline); // Free the line after printing to prevent memory leaks
+		free(newline); // Free the line after printing to prevent memory leaks
+
         a++; // Increment line counter
         b++; // Increment line number
     }
+	free(temp); // Free the line after printing to prevent memory leaks
     close(file); // Close the file descriptor
     return (0); // Return success
 }
