6a867b30f260ab06a3b661155727e0d75620786f
Eduardo Linux Acer <tardineduardo@gmail.com>
2024-08-04 11:57:20 +0200

    heap vs. stack

--- get_next_line.c
+++ get_next_line.c
@@ -1,29 +1,31 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-// static char *read_loop(int fd, char *nextline)
-// {
-// 	char		*buffer;
-// 	char		*temp_read_loop;
-// 	size_t		size_read;
+static char *read_loop_heap(int fd, char *nextline)
+{
+	char		*buffer;
+	char		*temp_read_loop;
+	size_t		size_read;
 
-// 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 
-// 	while(!ft_strchr(nextline, '\n'))
-// 	{
-// 		size_read = read(fd, buffer, BUFFER_SIZE);
-// 		buffer[size_read] = 0; // CHECAR ISSO!!!!
-// 		temp_read_loop = nextline;
-// 		nextline = ft_strjoin(nextline, buffer);
-// 		free(temp_read_loop);	
-// 		if (size_read < BUFFER_SIZE)
-// 			break;
-// 	}
-// 	free(buffer);
-// 	return(nextline);
-// }
+	while(!ft_strchr(nextline, '\n'))
+	{
+		size_read = read(fd, buffer, BUFFER_SIZE);
+		buffer[size_read] = 0; // CHECAR ISSO!!!!
+		temp_read_loop = nextline;
+		nextline = ft_strjoin(nextline, buffer);
+		free(temp_read_loop);
+		temp_read_loop = NULL;
+		if (size_read < BUFFER_SIZE)
+			break;
+	}
+	free(buffer);
+	buffer = NULL;
+	return(nextline);
+}
 
-static char *read_loop(int fd, char *nextline)
+static char *read_loop_stack(int fd, char *nextline)
 {
 	char		buffer[BUFFER_SIZE+1];
 	char		*temp_read_loop;
@@ -35,17 +37,15 @@ static char *read_loop(int fd, char *nextline)
 		buffer[size_read] = 0; // CHECAR ISSO!!!!
 		temp_read_loop = nextline;
 		nextline = ft_strjoin(nextline, buffer);
-		free(temp_read_loop);	
+		free(temp_read_loop);
+		temp_read_loop = NULL;
 		if (size_read < BUFFER_SIZE)
 			break;
 	}
 	return(nextline);
 }
 
-
-
-
- static void	ft_extract_remain(char *nextline, char *remainder)
+static void	ft_extract_remain(char *nextline, char *remainder)
 {
 	int		a;
 
@@ -63,14 +63,20 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)		
 		return (NULL);
+	if (read(fd, 0, 0) == -1)
+		return (NULL);	
 	nextline = ft_strdup(remainder);
 	remainder[0] = 0;
 	if (!nextline)
 		return (NULL);
-	nextline = read_loop(fd, nextline);
+	if (BUFFER_SIZE > 20000)
+		nextline = read_loop_heap(fd, nextline);	
+	if (BUFFER_SIZE <= 20000)
+	nextline = read_loop_stack(fd, nextline);
 	if (!nextline || nextline[0] == 0)
 	{
 		free(nextline);
+		nextline = NULL;
 		return (NULL);
 	}
 	if (ft_strchr(nextline, '\n'))
@@ -79,29 +85,29 @@ char	*get_next_line(int fd)
 }
 
 
-int main(void)
-{
-    int file = open("test1.txt", O_RDONLY); // Open a file for reading
-    int a = 0; // Counter for lines
-    int b = 1; // Line number for printing
-    char *newline; // Pointer to store the line read
-	char *tmp;
-	newline = malloc (1*1);
-	tmp = newline;
-    while(newline) 
-    {
-        newline = get_next_line(file); // Get the next line from the file
-        if (newline == NULL) // If NULL is returned (EOF or error)
-		{
-			break; // Exit the loop
-		}
-        printf("newline %d -> %s", b, newline); // Print the line
-		free(newline); // Free the line after printing to prevent memory leaks
+// int main(void)
+// {
+//     int file = open("test1.txt", O_RDONLY); // Open a file for reading
+//     int a = 0; // Counter for lines
+//     int b = 1; // Line number for printing
+//     char *newline; // Pointer to store the line read
+// 	char *tmp;
+// 	newline = malloc (1*1);
+// 	tmp = newline;
+//     while(newline) 
+//     {
+//         newline = get_next_line(file); // Get the next line from the file
+//         if (newline == NULL) // If NULL is returned (EOF or error)
+// 		{
+// 			break; // Exit the loop
+// 		}
+//         printf("newline %d -> %s", b, newline); // Print the line
+// 		free(newline); // Free the line after printing to prevent memory leaks
 
-        a++; // Increment line counter
-        b++; // Increment line number
-    }
-	free(tmp); // Free the line after printing to prevent memory leaks
-	tmp = NULL;
-    return (0); // Return success
-}
+//         a++; // Increment line counter
+//         b++; // Increment line number
+//     }
+// 	free(tmp); // Free the line after printing to prevent memory leaks
+// 	tmp = NULL;
+//     return (0); // Return success
+// }
